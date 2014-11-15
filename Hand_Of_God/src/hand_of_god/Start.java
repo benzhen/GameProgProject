bcc308250698d621d4bacf351917c6f608dcbeb3
Benny Zhen <bluehelper358@gmail.com>
2014-11-15 12:32:18 -0500

    New repository with old repository files

---
+++ Hand_Of_God/src/hand_of_god/Start.java
@@ -0,0 +1,17 @@
+package hand_of_god;
+
+/*
+ * 
+ * This class starts the game, if you want to test something
+ * just run this class.
+ * 
+ * 
+*/
+public class Start {
+
+	public static void main(String[] args) {
+		Playing_Field playing_field = new Playing_Field();
+
+	}
+
+}
